e09d6202138f5a4da320bd80017e8f3220dd9cce
Isabel Elise <80064876+isabel-elise@users.noreply.github.com>
2022-12-02 14:42:42 +0000

    Commit final

--- Main.java
+++ Main.java
@@ -3,13 +3,10 @@
 * Prof. Marco Tulio Valente
 * 
 * Roteiro Prático sobre refactoring
-* 
-* Classes iniciais do sistema de videolocadora
-*
 */
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Apenas um exemplo de refatoração...");
+    
   }
 }
